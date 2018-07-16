eaaed3a4c7c48226cb3569b83557927a2cbe2a8a
lavanyaanjulingam <41105309+lavanyaanjulingam@users.noreply.github.com>
2018-07-16 15:04:49 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Repeated {
+	public static void main(String[] args){
+		int i;
+		Scanner in=new Scanner(System.in);
+		for(i=0;i<2;i++){
+			System.out.println("hello");
+			
+			
+		}
+		
+	}
+
+}
