54d5e2deb15472bc6d3633443be659d5c60b8fb8
lavanyaanjulingam <41105309+lavanyaanjulingam@users.noreply.github.com>
2018-07-20 11:51:02 +0530

    Create dot.java

---
+++ dot.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class dot {
+	public static void main(String[] args){
+		String shalu;
+		Scanner in=new Scanner(System.in);
+		shalu=in.nextLine();
+		System.out.println(shalu+".");
+	}
+
+}
