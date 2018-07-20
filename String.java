28e2bb125572d1d35b7d9299dad7fcd440cbff15
lavanyaanjulingam <41105309+lavanyaanjulingam@users.noreply.github.com>
2018-07-20 09:44:17 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class shalu {
+public static void main(String[] args){
+		int i;
+		Scanner in=new Scanner(System.in);
+		String str=in.nextLine();
+		StringBuffer ds=new StringBuffer(str);
+		System.out.println(ds.reverse());
+		
+		}
+		
+		
+	}
+
+
